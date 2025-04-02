abd6273289ef8202a2bd56ec3763bc44f86ce6f4
unknown <yesitlabs.karankumar@gmail.com>
2025-04-02 17:07:44 +0530

    update code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4">
-      <div className="flex justify-between items-center px-10">
+      <div className="flex justify-between items-center md:px-10">
         <div className="text-2xl font-bold"> Deva Gallery</div>
-        <nav className="space-x-8">
+        <nav className="space-x-4 md:space-x-8">
           <a href="#products" className="hover:text-gray-400">Products</a>
           <a href="#contact" className="hover:text-gray-400">Contact Us</a>
         </nav>
